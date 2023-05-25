54ca33fd5877065e277afa60d015737ff2acebd6
shahkalp09 <133859153+shahkalp09@users.noreply.github.com>
2023-05-25 16:13:24 +0530

    inference.py

--- inference.py
+++ inference.py
@@ -4,6 +4,8 @@ import numpy as np
 from torch.nn.utils.rnn import pad_sequence
 from sklearn.preprocessing import LabelEncoder
 import pandas as pd
+from model import CNN
+
 # Read the CSV file
 df = pd.read_csv("C:\\Users\\Admin\\OneDrive\\Desktop\\internship\\language.csv")
 
@@ -22,25 +24,6 @@ def text_to_char_embeddings(text):
     char_embeddings = [char_to_index[char] for char in text]
     return torch.tensor(char_embeddings, dtype=torch.float32)
 
-# CNN model class
-class CNN(nn.Module):
-    def __init__(self, num_classes, vocab_size):
-        super(CNN, self).__init__()
-        self.conv1 = nn.Conv1d(1, 128, kernel_size=2)
-        self.conv2 = nn.Conv1d(128, 256, kernel_size=2)
-        self.fc = nn.Linear(256, num_classes)
-        self.vocab_size = vocab_size
-
-    def forward(self, x):
-        x = x.unsqueeze(1)  # Add a channel dimension
-        x = self.conv1(x)
-        x = nn.ReLU()(x)
-        x = self.conv2(x)
-        x = nn.ReLU()(x)
-        x = nn.AdaptiveMaxPool1d(1)(x)
-        x = x.squeeze(2)
-        x = self.fc(x)
-        return x
 
 # Initialize the CNN model
 num_classes = len(label_encoder.classes_)
